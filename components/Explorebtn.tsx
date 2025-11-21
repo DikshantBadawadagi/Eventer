0e957a78f99cb724331be6609003b572cf3d10a6
DikshantBadawadagi <dikshantbadawadagi@gmail.com>
2025-11-21 12:50:58 +0530

    setup + bg

---
+++ components/Explorebtn.tsx
@@ -0,0 +1,20 @@
+"use client"
+import Image from 'next/image'; 
+import { useRouter } from 'next/navigation'; 
+
+const Explorebtn = () => {
+  const router = useRouter();
+  
+  const handleClick = () => {
+    router.push('#events'); 
+  };
+
+  return (
+    <button type="button" id="button" className="mt-7 mx-auto btn" onClick={handleClick}>
+      Explore Now
+      <Image src="/icons/arrow-down.svg" alt="arrow-down" width={24} height={24}/>
+    </button>
+  );
+}
+
+export default Explorebtn;
